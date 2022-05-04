bd3b0109f23a3d2d45dbaa5c78060f67941d0aa6
Reese Liu <420221855@qq.com>
2022-05-04 19:06:07 +0800

    feat:修改项目名称

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/demo01'),
+      dir: require('path').join(__dirname, './coverage/reese-blog'),
       subdir: '.',
       reporters: [
         { type: 'html' },
